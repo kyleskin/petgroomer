1a495095cd0698209677cd0efffc8cfd411adf02
Kyle Skinner <kyleskinner@Kyles-MacBook-Pro.local>
2022-01-02 18:38:17 -0700

    initial setup

---
+++ PetGroomer/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.HttpOverrides;
+using PetGroomer.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
