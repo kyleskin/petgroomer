46a605ea50dcb53eb9b08806dfd3e9d3d8daeb96
Kyle Skinner <kyleskin@gmail.com>
2022-01-15 17:18:38 -0700

    groomer controller - create groomer

--- PetGroomer.Presentation/Controllers/GroomersController.cs
+++ PetGroomer.Presentation/Controllers/GroomersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 
 namespace PetGroomer.Presentation.Controllers
 {
@@ -20,6 +21,24 @@ namespace PetGroomer.Presentation.Controllers
         {
             var groomers = _service.GroomerService.GetGroomers(salonId, trackChanges: false);
             return Ok(groomers);
+        }
+
+        [HttpGet("{id:guid}", Name = "GetGroomerInSalon")]
+        public IActionResult GetGroomerInSalon(Guid salonId, Guid id)
+        {
+            var groomer = _service.GroomerService.GetGroomer(salonId, id, trackChanges: false);
+            return Ok(groomer);
+        }
+
+        [HttpPost]
+        public IActionResult CreateGroomerInSalon(Guid salonId, [FromBody] GroomerForCreationDto groomer)
+        {
+            if (groomer is null)
+                return BadRequest("GroomerForCreationDto is null.");
+
+            var groomerToReturn = _service.GroomerService.CreateGroomerInSalon(salonId, groomer, trackChages: false);
+
+            return CreatedAtRoute("GetGroomerInSalon", new { salonId, id = groomerToReturn.Id }, groomerToReturn);
         }
     }
 }
