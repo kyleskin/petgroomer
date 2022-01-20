e702e8949c28c66906b3ae919fa1b03a5af38f5e
Kyle Skinner <kyleskin@gmail.com>
2022-01-20 15:02:21 -0700

    finished refresh token configuation

---
+++ Entities/Exceptions/BadRequestException.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entities.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        protected BadRequestException(string message)
+            : base(message)
+        {}
+    }
+}
--- PetGroomer/Migrations/20220120211455_AdditionalUserFieldsForRefreshToken.Designer.cs
+++ PetGroomer/Migrations/20220120211455_AdditionalUserFieldsForRefreshToken.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository;
 
@@ -10,9 +11,10 @@ using Repository;
 namespace PetGroomer.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20220120211455_AdditionalUserFieldsForRefreshToken")]
+    partial class AdditionalUserFieldsForRefreshToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
